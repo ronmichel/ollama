60e4e18f638179adfa88d21909febd62efafc907
A-Akhil <akhilrahul70@gmail.com>
2025-10-22 08:42:52 +0530

    remove TODO comment for embedding support in Execute function

--- runner/ollamarunner/runner.go
+++ runner/ollamarunner/runner.go
@@ -1361,7 +1361,6 @@ func Execute(args []string) error {
 	defer listener.Close()
 
 	mux := http.NewServeMux()
-	// TODO: support embeddings
 	mux.HandleFunc("GET /info", server.info)
 	mux.HandleFunc("POST /load", server.load)
 	mux.HandleFunc("POST /embedding", server.embeddings)
